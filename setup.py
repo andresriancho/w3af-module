4fe8b86bc7ce173a9d4f41a8e719e65d723b782a
Andres Riancho <andres.riancho@gmail.com>
2013-07-08 14:22:48 -0300

    Fixed the filtering logic for urls

--- setup.py
+++ setup.py
@@ -29,14 +29,15 @@ def get_pip_requirements():
     :return: A list with the names of the pip packages used in w3af (both
              the console and GUI versions).
     '''
-    return [p.package_name for p in PIP_PACKAGES if 'git://' not in p.package_name]
+    return [p.package_name for p in PIP_PACKAGES\
+            if 'git://' not in p.package_name]
 
 def get_pip_git_requirements():
     '''
     :return: The requirements that should be installed from git
     '''
     git_packages =  [p.package_name for p in PIP_PACKAGES \
-                     if 'git://' not in p.package_name]
+                     if 'git://' in p.package_name]
 
     return [pkg_url.replace('git+git://', 'http://') for pkg_url in git_packages]
 
