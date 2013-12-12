12014beab1b6af312533013d1d20d54c2225b727
Andres Riancho <andres.riancho@gmail.com>
2013-12-12 09:01:44 -0300

    Fixing test after updating to new moth which changed some things

--- mod_utils/pip.py
+++ mod_utils/pip.py
@@ -6,14 +6,16 @@ def get_pip_requirements():
     :return: A list with the names of the pip packages used in w3af (both
              the console and GUI versions).
     '''
-    return [p.package_name for p in PIP_PACKAGES\
-            if 'git://' not in p.package_name]
+    return [p.package_name for p in PIP_PACKAGES if not p.is_git]
 
 def get_pip_git_requirements():
     '''
     :return: The requirements that should be installed from git
     '''
-    git_packages =  [p.package_name for p in PIP_PACKAGES \
-                     if 'git://' in p.package_name]
+    git_packages = [p for p in PIP_PACKAGES if p.is_git]
 
-    return [pkg_url.replace('git+git://', 'https://') for pkg_url in git_packages]
+    for to_replace in ('git+git://', 'git+https://'):
+        for package in git_packages:
+            package.git_src = package.git_src.replace(to_replace, 'https://')
+
+    return [package.git_src for package in git_packages]
