da44cae810cb7840e6187a85d91afd46bf52ecf0
Andres Riancho <andres.riancho@gmail.com>
2013-12-12 09:35:53 -0300

    Fixed pip install, can't clone from the local repository since it's a shallow one

--- tests/test_pip_install.py
+++ tests/test_pip_install.py
@@ -6,4 +6,4 @@ from tests.base import BaseInstallMixin
 class TestPIPInstall(BaseInstallMixin, unittest.TestCase):
 
     UNINSTALL_CMD = 'pip uninstall -y w3af'
-    INSTALL_CMD = 'pip install git+file:///home/ubuntu/w3af-module/'
+    INSTALL_CMD = 'pip install git+https://github.com/andresriancho/w3af-module.git'
