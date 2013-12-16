e85b97b2e44cb55da002612e3f89e2c891934782
Andres Riancho <andres.riancho@gmail.com>
2013-12-16 17:50:44 -0300

    Including profiles as data files

--- mod_utils/gen_data_files.py
+++ mod_utils/gen_data_files.py
@@ -1,11 +1,40 @@
 import os
 
+EXCLUDE_EXTENSIONS = ('.py', '.pyc')
+PROFILE_ROOT_SRC = 'w3af-repo/profiles/'
+PROFILE_ROOT_DST = 'w3af/profiles/'
 
-def gen_data_files(*dirs):
+
+def gen_data_files(_dir):
+    result = []
+    result.extend(gen_profile_files())
+    result.extend(gen_generic_data_files(_dir))
+    return result
+
+
+def gen_profile_files():
+    path_join = lambda f: PROFILE_ROOT_SRC + "/" + f
+
+    profile_data = [(PROFILE_ROOT_DST,
+                     map(path_join, os.listdir(PROFILE_ROOT_SRC)))]
+    return profile_data
+
+
+def gen_generic_data_files(_dir):
     results = []
 
-    for src_dir in dirs:
-        for root, dirs, files in os.walk(src_dir):
-            results.append((root, map(lambda f: root + "/" + f, files)))
-            
+    path_join = lambda f: root + "/" + f
+
+    def file_filter(fname):
+        for exclude_ext in EXCLUDE_EXTENSIONS:
+            if fname.endswith(exclude_ext):
+                return False
+
+        return True
+
+    for root, dirs, files in os.walk(_dir):
+        filtered_files = filter(file_filter, files)
+        if filtered_files:
+            results.append((root, map(path_join, filtered_files)))
+
     return results
