487398c52d7cefc4d8c4ebc22047876e22f7d447
Andres Riancho <andres.riancho@gmail.com>
2013-12-16 19:25:16 -0300

    Fixing test

--- mod_utils/tests/test_gen_data_files.py
+++ mod_utils/tests/test_gen_data_files.py
@@ -9,14 +9,13 @@ class TestGenDataFiles(unittest.TestCase):
         
         directories = [d for (d, _) in result]
         
-        self.assertIn('mod_utils', directories)
         self.assertIn('mod_utils/tests', directories)
         
         for directory, file_list in result:
-            if directory == 'utils/tests':
-                
-                self.assertIn('utils/tests/test_pip.py', file_list)
-                self.assertIn('utils/tests/test_gen_data_files.py', file_list)
-                
-                # Make sure that all files are there, not only py
-                self.assertIn('utils/tests/data_test.bin', file_list)
+            if directory == 'mod_utils/tests':
+
+                # data files should not contain python files
+                self.assertNotIn('mod_utils/tests/test_pip.py', file_list)
+
+                # Make sure that non py files are there
+                self.assertIn('mod_utils/tests/data_test.bin', file_list)
