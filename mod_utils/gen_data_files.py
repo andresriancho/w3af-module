1ee2df3a6d3f29653de823cb3bd0432463c4fc4e
Andres Riancho <andres.riancho@gmail.com>
2013-12-16 16:32:23 -0300

    Cosmetic

--- mod_utils/gen_data_files.py
+++ mod_utils/gen_data_files.py
@@ -6,6 +6,6 @@ def gen_data_files(*dirs):
 
     for src_dir in dirs:
         for root, dirs, files in os.walk(src_dir):
-            results.append((root, map(lambda f:root + "/" + f, files)))
+            results.append((root, map(lambda f: root + "/" + f, files)))
             
     return results
