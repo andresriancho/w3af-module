c7b6ea10fc5211cbd067c0ebd95db1bb52908964
Andres Riancho <andres.riancho@gmail.com>
2013-12-17 09:48:15 -0300

    Removing flag which wasn't working because it required --root or --record

--- tests/test_setup_install.py
+++ tests/test_setup_install.py
@@ -6,4 +6,4 @@ from tests.base import BaseInstallMixin
 
 class TestSetupInstall(BaseInstallMixin, unittest.TestCase):
 
-    INSTALL_CMD = '%s setup.py install --single-version-externally-managed' % sys.executable
+    INSTALL_CMD = '%s setup.py install' % sys.executable
