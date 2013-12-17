4887f85d83f1a4c0c6d0777e756218dc563bfa32
Andres Riancho <andres.riancho@gmail.com>
2013-12-17 13:22:07 -0300

    Adding test for local pip install

--- tests/test_pip_install.py
+++ tests/test_pip_install.py
@@ -3,6 +3,11 @@ import unittest
 from tests.base import BaseInstallMixin
 
 
-class TestPIPInstall(BaseInstallMixin, unittest.TestCase):
+class TestPIPInstallRemote(BaseInstallMixin, unittest.TestCase):
 
     INSTALL_CMD = 'pip install git+https://github.com/andresriancho/w3af-module.git'
+
+
+class TestPIPInstallLocal(BaseInstallMixin, unittest.TestCase):
+
+    INSTALL_CMD = 'pip install .'
