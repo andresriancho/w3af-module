7b1d619ba6e4400a3777abbf26e38e1f9a64ee5f
Andres Riancho <andres.riancho@gmail.com>
2013-12-17 14:32:43 -0300

    Fixing test

--- mod_utils/tests/test_pip.py
+++ mod_utils/tests/test_pip.py
@@ -7,7 +7,8 @@ class TestPip(unittest.TestCase):
     def test_get_pip_git_requirements(self):
         pips = get_pip_git_requirements()
         
-        self.assertIn('https://github.com/ramen/phply.git#egg=phply', pips)
+        self.assertIn('https://github.com/andresriancho/phply/archive/0.9.tar.gz#egg=phply-dev',
+                      pips)
     
     def test_get_pip_requirements(self):
         pips = get_pip_requirements()
