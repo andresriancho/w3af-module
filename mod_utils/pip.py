e76a47529f001c11b9d90bfbd6914738eaddef10
Andres Riancho <andres.riancho@gmail.com>
2014-06-04 16:54:45 -0300

    Pointing to the new location of PIP dependencies

--- mod_utils/pip.py
+++ mod_utils/pip.py
@@ -1,20 +1,20 @@
-from w3af.core.controllers.dependency_check.platforms.current_platform import PIP_PACKAGES
+from w3af.core.controllers.dependency_check.requirements import CORE_PIP_PACKAGES
 
 
 def get_pip_requirements():
-    '''
+    """
     :return: A list with the names of the pip packages used in w3af (both
              the console and GUI versions).
-    '''
-    return ['%s==%s' % (p.package_name, p.package_version) for p in PIP_PACKAGES]
+    """
+    return ['%s==%s' % (p.package_name, p.package_version) for p in CORE_PIP_PACKAGES]
 
 
 def get_pip_git_requirements():
-    '''
+    """
     :return: The requirements that should be installed from git
-    '''
+    """
     result = []
-    git_packages = [p for p in PIP_PACKAGES if p.is_git]
+    git_packages = [p for p in CORE_PIP_PACKAGES if p.is_git]
 
     for package in git_packages:
         for to_replace in ('git+git://', 'git+https://'):
