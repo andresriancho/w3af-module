0e5064a3fb9fcd83ce20e512f052f32b024db49b
Andres Riancho <andres.riancho@gmail.com>
2014-08-14 14:49:36 -0300

    .

--- mod_utils/ci/update-branch.sh
+++ mod_utils/ci/update-branch.sh
@@ -14,16 +14,16 @@ if [ $CIRCLE_BRANCH == 'master' ]; then
     git checkout w3af-master
     git pull -v
 
+    git checkout $CIRCLE_BRANCH
     git read-tree --prefix=w3af-repo/ -u w3af-master
 
-    git checkout master
     git merge --squash -s subtree --no-commit w3af-master
 else
     git checkout w3af-develop
     git pull -v
 
+    git checkout $CIRCLE_BRANCH
     git read-tree --prefix=w3af-repo/ -u w3af-develop
 
-    git checkout $CIRCLE_BRANCH
     git merge --squash -s subtree --no-commit w3af-develop
 fi
