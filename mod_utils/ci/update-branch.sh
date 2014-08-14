a7b97b2c3321340c5b1ff1fae2e1d16d7d5551c1
Andres Riancho <andres.riancho@gmail.com>
2014-08-14 14:19:27 -0300

    Adding debugging information to make sure we pull from the right repo

--- mod_utils/ci/update-branch.sh
+++ mod_utils/ci/update-branch.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -x
 
 if [ $CIRCLE_BRANCH == 'master' ]; then
+    git remote
     git checkout w3af-master
-    git pull
+    git pull -v
 
     git checkout master
     git merge --squash -s subtree --no-commit w3af-master
 else
+    git remote
     git checkout w3af-develop
-    git pull
+    git pull -v
 
     git checkout develop
     git merge --squash -s subtree --no-commit w3af-develop
