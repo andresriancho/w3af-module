4a29ecbcb3bb0f6ebb2a65f80ce842a6036e52b9
Andres Riancho <andres.riancho@gmail.com>
2014-08-14 14:32:35 -0300

    Quiet git rm

--- mod_utils/ci/update-branch.sh
+++ mod_utils/ci/update-branch.sh
@@ -8,10 +8,10 @@ git branch -D w3af-develop
 git checkout -b w3af-master w3af/master
 git checkout -b w3af-develop w3af/develop
 
-if [ $CIRCLE_BRANCH == 'master' ]; then
-    git checkout master
-    git rm -r w3af-repo
+git checkout $CIRCLE_BRANCH
+git rm -r -q w3af-repo
 
+if [ $CIRCLE_BRANCH == 'master' ]; then
     git read-tree --prefix=w3af-repo/ -u w3af-master
     git checkout w3af-master
     git pull -v
@@ -19,9 +19,6 @@ if [ $CIRCLE_BRANCH == 'master' ]; then
     git checkout master
     git merge --squash -s subtree --no-commit w3af-master
 else
-    git checkout $CIRCLE_BRANCH
-    git rm -r w3af-repo
-
     git read-tree --prefix=w3af-repo/ -u w3af-develop
     git checkout w3af-develop
     git pull -v
