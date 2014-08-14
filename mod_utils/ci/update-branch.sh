60649de906f11dc145874b8b74e28e2ee69f329b
Andres Riancho <andres.riancho@gmail.com>
2014-08-14 14:28:07 -0300

    Ugly git voodoo

--- mod_utils/ci/update-branch.sh
+++ mod_utils/ci/update-branch.sh
@@ -1,17 +1,30 @@
 #!/bin/bash -x
 
+git remote add -f w3af git://github.com/andresriancho/w3af.git
+
+git branch -D w3af-master
+git branch -D w3af-develop
+git checkout -b w3af-master w3af/master
+git checkout -b w3af-develop w3af/develop
+
 if [ $CIRCLE_BRANCH == 'master' ]; then
-    git remote
+    git checkout master
+    git rm -r w3af-repo
+
+    git read-tree --prefix=w3af-repo/ -u w3af-master
     git checkout w3af-master
     git pull -v
 
     git checkout master
     git merge --squash -s subtree --no-commit w3af-master
 else
-    git remote
+    git checkout $CIRCLE_BRANCH
+    git rm -r w3af-repo
+
+    git read-tree --prefix=w3af-repo/ -u w3af-develop
     git checkout w3af-develop
     git pull -v
 
-    git checkout develop
+    git checkout $CIRCLE_BRANCH
     git merge --squash -s subtree --no-commit w3af-develop
 fi
