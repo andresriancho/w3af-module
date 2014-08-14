4ce4061d898876f07d8f8847c6c1c11b2002e879
Andres Riancho <andres.riancho@gmail.com>
2014-08-14 14:43:16 -0300

    Fixing with the objective of getting the git voodoo magic working

--- mod_utils/ci/update-branch.sh
+++ mod_utils/ci/update-branch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
 git remote add -f w3af git://github.com/andresriancho/w3af.git
-git fetch w3af
 
 git branch -D w3af-master
 git branch -D w3af-develop
@@ -12,16 +11,18 @@ git checkout $CIRCLE_BRANCH
 git rm -r -q w3af-repo
 
 if [ $CIRCLE_BRANCH == 'master' ]; then
-    git read-tree --prefix=w3af-repo/ -u w3af-master
     git checkout w3af-master
     git pull -v
+
+    git read-tree --prefix=w3af-repo/ -u w3af-master
 
     git checkout master
     git merge --squash -s subtree --no-commit w3af-master
 else
-    git read-tree --prefix=w3af-repo/ -u w3af-develop
     git checkout w3af-develop
     git pull -v
+
+    git read-tree --prefix=w3af-repo/ -u w3af-develop
 
     git checkout $CIRCLE_BRANCH
     git merge --squash -s subtree --no-commit w3af-develop
