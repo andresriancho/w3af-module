f92b6c247275c73760e4427c38bf96365bb615cd
CircleCI <ci@circleci.com>
2015-03-30 23:19:54 +0000

    Merging latest w3af(develop) into w3af-module(develop) [ci skip]

--- w3af-repo/w3af/core/data/constants/tests/test_vulns.py
+++ w3af-repo/w3af/core/data/constants/tests/test_vulns.py
@@ -33,31 +33,113 @@ from w3af.core.controllers.ci.constants import ARTIFACTS_DIR
 class TestVulnsConstants(unittest.TestCase):
     
     LOCATION = os.path.join(ROOT_PATH, 'core', 'data', 'constants', 'vulns.py')
-    
-    def test_no_duplicated_ids(self):
+
+    def get_all_vulnerability_names(self):
         # Just skip the entire license header
-        vulns_file = file(self.LOCATION) 
+        vulns_file = file(self.LOCATION)
         for _ in xrange(21):
             vulns_file.readline()
-            
-        vuln_id_list = re.findall('(\d+):', vulns_file.read())
-        filtered = set()
-        dups = set()
-        
-        for vuln_id in vuln_id_list:
-            if vuln_id in filtered:
-                dups.add(vuln_id)
-            
-            filtered.add(vuln_id)
 
-        self.assertEquals(set([]), dups)
-    
-    def test_no_empty(self):
-        items = VULNS.items()
-        empty_values = set([(key, val) for (key, val) in items if not val])
-        self.assertEqual(set([]), empty_values)
+        return re.findall('[\'"](.*?)[\'"] ?:', vulns_file.read())
 
-    @attr('ci_fails')
+    def test_vulnerability_names_unique(self):
+        dups = []
+        vuln_names = self.get_all_vulnerability_names()
+
+        for name in vuln_names:
+            if vuln_names.count(name) > 1 and name not in dups:
+                dups.append(name)
+
+        self.assertEqual(dups, [])
+
+    def get_all_plugins_source(self):
+        plugins_path = os.path.join(ROOT_PATH, 'plugins')
+        vuln_template_path = os.path.join(ROOT_PATH, 'core', 'data', 'kb',
+                                          'vuln_templates')
+
+        all_plugin_sources = ''
+        for dir_name, subdir_list, file_list in os.walk(plugins_path):
+
+            if dir_name in ('test', 'tests'):
+                continue
+
+            for fname in file_list:
+                if not fname.endswith('.py'):
+                    continue
+
+                if fname.startswith('test_'):
+                    continue
+
+                if fname == '__init__.py':
+                    continue
+
+                full_path = os.path.join(plugins_path, dir_name, fname)
+
+                ignores = {'/attack/db/sqlmap/',
+                           '/attack/payloads/',
+                           '/plugins/tests/'}
+
+                should_continue = False
+                for ignore in ignores:
+                    if ignore in full_path:
+                        should_continue = True
+                        break
+
+                if should_continue:
+                    continue
+
+                all_plugin_sources += file(full_path).read()
+
+        for dir_name, subdir_list, file_list in os.walk(vuln_template_path):
+
+            for fname in file_list:
+                if not fname.endswith('.py'):
+                    continue
+
+                if fname.startswith('test_'):
+                    continue
+
+                if fname == '__init__.py':
+                    continue
+
+                full_path = os.path.join(vuln_template_path, dir_name, fname)
+                all_plugin_sources += file(full_path).read()
+
+        return all_plugin_sources
+
+    def test_all_vulnerability_names_from_db_are_used(self):
+        vuln_names = VULNS.keys()
+        all_plugin_sources = self.get_all_plugins_source()
+        missing_ignore = {'TestCase',
+                          'Blind SQL injection vulnerability'}
+
+        for vuln_name in vuln_names:
+            if vuln_name in missing_ignore:
+                continue
+
+            msg = '"%s" not in plugin sources' % vuln_name
+            self.assertIn(vuln_name, all_plugin_sources, msg)
+
+    def test_all_vulnerability_names_from_source_in_db(self):
+        vuln_names = VULNS.keys()
+        vuln_names_re = ' (Info|Vuln)\\(["\'](.*?)["\'] ?,.*?\\)'
+        all_plugin_sources = self.get_all_plugins_source()
+        vuln_names_in_source = re.findall(vuln_names_re, all_plugin_sources,
+                                          re.DOTALL)
+
+        extracted = []
+        not_in_db = []
+
+        for _type, vuln_title in vuln_names_in_source:
+            extracted.append(vuln_title)
+
+            if vuln_title not in vuln_names and vuln_title not in not_in_db:
+                not_in_db.append(vuln_title)
+
+        self.assertEqual(not_in_db, [])
+        self.assertGreater(len(extracted), 120, extracted)
+
+    @attr('ci_ignore')
     def test_vuln_updated(self):
         """
         Each time we call Info.set_name during a test (and only during tests,
